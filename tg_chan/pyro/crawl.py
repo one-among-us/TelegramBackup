89faddf70f5513e0c96ce07e3154d5c58e6cb595
Hykilpikonna <me@hydev.org>
2022-12-09 15:17:03 -0500

    [O] Move crawler

--- tg_chan/pyro/crawl.py
+++ tg_chan/pyro/crawl.py
@@ -1,21 +1,22 @@
 import argparse
 import asyncio
 from pathlib import Path
-from typing import BinaryIO
 
 import uvloop
-from hypy_utils.tqdm_utils import tmap
 from pyrogram import Client
-from pyrogram.types import User, Chat, Message, Photo
+from pyrogram.enums import MessageMediaType
+from pyrogram.types import User, Chat, Message
 from ruamel import yaml
 
-from tg_chan_api.download_media import download_media
+from tg_chan.pyro.convert import convert_text
+from ..convert_export import remove_nones
+from .download_media import download_media
 
 MEDIA_PATH = Path("media")
 uvloop.install()
 
 
-def load_config(path: str) -> dict[str, str]:
+def load_config(path: str) -> dict:
     with open(path, "r") as f:
         return yaml.safe_load(f)
 
@@ -40,19 +41,52 @@ def parse_msg(msg: Message) -> dict | None:
 
 
 def effective_text(msg: Message) -> str:
+    """
+    Get effective text of a message in HTML
+    """
     if msg.text:
-        return msg.text.markdown
+        return convert_text(msg.text, msg.text.entities)
     if msg.caption:
-        return msg.caption.markdown
+        return convert_text(msg.caption, msg.caption.entities)
     if msg.service:
         return str(msg.service)
+
+
+MEDIA_TYPE_MAP: dict[MessageMediaType, str] = {
+    MessageMediaType.STICKER: "sticker",
+    MessageMediaType.VOICE: "voice_message",
+    MessageMediaType.AUDIO: "audio_file",
+    MessageMediaType.ANIMATION: "animation",
+    MessageMediaType.VIDEO: "video_file",
+    MessageMediaType.VIDEO_NOTE: "video_file",
+
+    # TODO: Support these in web ui
+    MessageMediaType.CONTACT: "contact",
+    MessageMediaType.POLL: "poll",
+    MessageMediaType.WEB_PAGE: "web_page",
+    MessageMediaType.LOCATION: "location",
+    MessageMediaType.VENUE: "location"
+}
 
 
 def _download_media_helper(args: list) -> Path:
     return asyncio.run(download_media(app, *args))
 
 
-async def process_messages(msgs: list[Message]):
+def process_message(msg: Message) -> dict:
+    m = {
+        "id": msg.id,
+        "date": msg.date,
+        "text": effective_text(msg),
+        "author": msg.author_signature,
+        "views": msg.views,
+        "forwards": msg.forwards
+    }
+
+    return remove_nones(m)
+
+
+async def process_messages(msgs: list[Message], path: Path):
     # 1. Download media
     # media_msgs = [[m] for m in msgs if m.media]
     # fps: list[Path] = smap(_download_media_helper, media_msgs)
@@ -63,16 +97,26 @@ async def process_messages(msgs: list[Message]):
             fp = fp.absolute().relative_to(Path().absolute())
 
 
+async def process_chat(chat_id: int, path: Path):
+    chat: Chat = await app.get_chat(chat_id)
+    print(f"Chat obtained. Chat name: {chat.title} | Type: {chat.type} | ID: {chat.id}")
+
+    # Crawl messages
+    msgs = await app.get_messages(chat.id, range(1, 40))
+
+    # for m in msgs:
+    #     print(type(m.media))
+
+    # print('\n'.join([f'{m.id}: {effective_text(m)}' for m in msgs if not m.empty]))
+    print(msgs)
+    # await process_messages(msgs, path)
+
+
 async def run(app: Client):
     me: User = await app.get_me()
     print(f"Login success! ID: {me.id} | is_bot: {me.is_bot}")
-    chat: Chat = await app.get_chat(cfg['chat_id'])
-    print(f"Chat obtained. Chat name: {chat.title} | Type: {chat.type} | ID: {chat.id}")
-    # Bot
-    msgs = await app.get_messages(chat.id, range(1, 30))
-    # print('\n'.join([f'{m.id}: {effective_text(m)}' for m in msgs if not m.empty]))
-    print(msgs)
-    await process_messages(msgs)
+    for export in cfg["exports"]:
+        await process_chat(int(export["chat_id"]), Path(export["path"]))
 
 
 if __name__ == '__main__':
