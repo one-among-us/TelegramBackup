f2de42dd7a70736fd5d06f72f28c788bbb348fb1
Azalea Gui <me@hydev.org>
2023-03-06 14:51:46 -0500

    [F] Fix posts_to_feed and add atom generation

--- tgc/rss/posts_to_feed.py
+++ tgc/rss/posts_to_feed.py
@@ -1,8 +1,12 @@
+import html
 import json
 from dataclasses import dataclass
+from datetime import timezone
 from pathlib import Path
 
+from dateutil import parser
 from feedgen.feed import FeedGenerator
+from html2text import html2text
 
 
 @dataclass
@@ -11,6 +15,7 @@ class FeedMeta:
     link: str
     description: str
     language: str
+    image_url: str
 
 
 def posts_to_feed(path: Path, meta: FeedMeta):
@@ -23,17 +28,26 @@ def posts_to_feed(path: Path, meta: FeedMeta):
     fg = FeedGenerator()
     
     # Meta info
+    fg.id(meta.link)
     fg.title(meta.title)
     fg.link(href=meta.link, rel='alternate')
     fg.description(meta.description)
     fg.language(meta.language)
+    fg.image(meta.image_url)
 
     # Posts
     posts = json.loads((path / 'posts.json').read_text())
     for post in posts:
         fe = fg.add_entry()
-        fe.id(post['id'])
-        fe.description(post['text'])
-        fe.pubDate(post['date'])
+        fe.id(str(post['id']))
+        fe.title(f"{meta.title} #{post['id']}")
+        fe.link(href=f'{meta.link}?post={post["id"]}')
+        fe.updated(parser.parse(post['date']).replace(tzinfo=timezone.utc))
 
-    fg.rss_file(path / 'feed.xml')
+        # Escape HTML tags
+        text = html2text(post.get('text', post.get('caption', ''))).replace('\n', '<br>')
+        text = html.escape(text)
+        fe.description(text)
+
+    fg.rss_file(path / 'rss.xml', pretty=True)
+    fg.atom_file(path / 'atom.xml', pretty=True)
